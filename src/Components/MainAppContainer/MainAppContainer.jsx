a81a45c5b090277b4019c7b81465795baaa63a32
Ozod <o.bukhorizoda@mail.ru>
2023-11-17 16:12:06 +0500

    set proptypes validation

--- src/Components/MainAppContainer/MainAppContainer.jsx
+++ src/Components/MainAppContainer/MainAppContainer.jsx
@@ -1,4 +1,9 @@
 import styles from "./MainAppContainer.module.css";
+import PropTypes from "prop-types";
+
 export default function MainAppContainer({ children }) {
   return <div className={styles.main_app_container}>{children}</div>;
 }
+MainAppContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
